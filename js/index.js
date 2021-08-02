6ebe500c44b52e0832c5649cfc7ea6d802dc5a1a
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2021-08-01 23:07:21 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* function cadastrar(nome,email){
+    var nome = prompt("Qual o seu nome :");
+    var email = prompt("Qual o seu email ?");   
+    console.log("Nome: "+nome + "  Email: "+email);
+}
+
+cadastrar(); */
